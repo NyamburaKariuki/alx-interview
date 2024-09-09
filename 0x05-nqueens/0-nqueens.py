f3f4cfe3101b10d4b4827f8a8a54815ff9e81adf
NyamburaKariuki <prettimercii@gmail.cm>
2024-09-09 21:38:39 +0300

    n1ueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -9,11 +9,7 @@ where N must be an integer greater or equal to 4
     and exit with the status 1.
     If N is smaller than 4, print N must be at least 4, followed by a new
     line, and exit with the status 1.
-The program should print every possible solution to the problem.
-    One solution per line.
-    Format: see README.
-    You don’t have to print the solutions in a specific order.
-You are only allowed to import the sys module.
+
 """
 import sys
 
@@ -167,5 +163,5 @@ n = get_input()
 get_solutions()
 
 for solution in solutions:
-    
+
     print(solution)

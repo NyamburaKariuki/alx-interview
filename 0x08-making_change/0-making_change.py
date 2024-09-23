7116039ce17703176ecb390229a2542d893bcc23
NyamburaKariuki <prettimercii@gmail.cm>
2024-09-23 16:36:10 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,11 +2,13 @@
 """Module for Making Change.
 """
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize a list with a large number, which will represent an impossible total
+    """ Initialize a list with a large number, which will\
+    represent an impossible total"""
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # No coins are needed to make the total 0
 
@@ -15,5 +17,6 @@ def makeChange(coins, total):
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    # If the total is still infinity, then it's not possible to make the total with given coins
+    """If the total is still infinity, then it's not\ 
+    possible to make the total with given coins"""
     return dp[total] if dp[total] != float('inf') else -1
